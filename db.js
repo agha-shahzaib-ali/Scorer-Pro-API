7df22781c0f76419abde657e252fbe608eb4365c
Agha Shahzaib Ali <shahzaibali507007@gmail.com>
2022-10-08 06:39:50 +0500

    credentials removed

--- db.js
+++ db.js
@@ -1,7 +1,4 @@
 // getting-started.js
-// const PASSWORD = "pieas2019";
-// const USERNAME = "agha-shahzaib-ali";
-// const DB_NAME = "scorer-pro";
 // Configuration for dotenv pkg and file
 require("dotenv").config();
 const { DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env; // Object Destructuring
@@ -20,5 +17,4 @@ async function main() {
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
 
-// mongodb+srv://agha-shahzaib-ali:<password>@cluster0.rhra5x8.mongodb.net/?retryWrites=true&w=majority
-// euK2zXBuSvoqDJMr
+// mongodb+srv://<username>:<password>@cluster0.rhra5x8.mongodb.net/<database_name>?retryWrites=true&w=majority
